7357e6014f44b25cdc952f1bb892dae48a04e97b
ParasMahla <parasmahla444@gmail.com>
2025-06-30 11:27:41 +0530

    new files added

---
+++ functions.js
@@ -0,0 +1,17 @@
+// //arrow function
+// cosnt hello = (fruit) => {
+//   console.log(fruit);
+// }
+
+// hello ("apple");
+
+
+let className = "bca";
+let hello = `my class is ${className}`
+console.log(hello);
+
+7let classname = ["bca", "btech", "mca"];
+for (let i = 0; i < classname.length; i++) {
+  let hello = `my class is ${classname[i]}`;
+  console.log(hello);
+}
